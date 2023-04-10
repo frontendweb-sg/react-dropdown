46d829bd3589f4c2ef83dc77dc59bdf9b0413e56
startx88 <arya.creativemind@gmail.com>
2023-04-10 13:14:55 +0530

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import pkg from './package.json' assert { type: 'json' };
+
+const isProd = process.env.ROLLUP_WATCH ?? false;
+const ext = ['.js', '.jsx', '.ts', '.tsx', '.json'];
+
+/**
+ * @type{import('rollup').RollupOptions}
+ */
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: isProd,
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+        sourcemap: isProd,
+      },
+      {
+        file: 'dist/umd/index.js',
+        format: 'umd',
+        sourcemap: isProd,
+        name: 'ReactDropdown',
+        global: {
+          react: '^18.2.0',
+        },
+      },
+    ],
+    plugins: [
+      commonjs(),
+      resolve({ extensions: ext }),
+      typescript({ tsconfig: './tsconfig.json' }),
+      !isProd && terser(),
+    ],
+  },
+];
